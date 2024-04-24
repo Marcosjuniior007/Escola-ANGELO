6f9f0ba6507c61376ff1fb451512b7217363d6b9
Marcos Junior <167099991+Marcosjuniior007@users.noreply.github.com>
2024-04-24 08:00:22 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,25 @@
+ListaNomes = []
+
+
+ListaNomes.append("marcos")
+ListaNomes.append("tamires")
+ListaNomes.append("edson")
+
+print(ListaNomes)
+print(ListaNomes([1]))
+
+for nome in ListaNomes
+   print(nome)
+
+   #DICIONARIO
+   primeiroDicionario =  {}
+
+   primeiroDicionario['nome'] = "marcos"
+   primeiroDicionario['tipo'] = 'estudante'
+   primeiroDicionario['numero'] = 16
+  print(primeiroDicionario)
+ print(primeiroDicionario["tipo"])
+
+# Anatomia
+# Lista[indice] = Valor
+# dicionario[chave] = Valor
